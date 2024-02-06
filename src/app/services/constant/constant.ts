96a2fa37515b3a0f98691bce29d54fabb0e8a1a6
Santosh Singh <santosh007bca@gmail.com>
2024-02-06 17:52:01 +0530

    ecomm project

---
+++ src/app/services/constant/constant.ts
@@ -0,0 +1,8 @@
+export const Constant = {
+    API_END_POINT:'https://onlinetestapi.gerasim.in/api/Ecomm/',
+    METHODS:{
+        GET_ALL_PRODUCT: 'GetAllProducts',
+        GET_ALL_CATEGORY: 'GetAllCategory',
+        CREATE_PRODUCT: 'CreateProduct'
+    }
+}
